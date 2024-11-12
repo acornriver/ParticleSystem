8a01f6f8f160c25b5ed73147ae89f69f94b56563
acornriver <kmicky0806@gmail.com>
2024-11-12 16:00:10 +0900

    add systems

--- sketch.js
+++ sketch.js
@@ -1,15 +1,31 @@
-let system;
+// let system;
+let systems = [];
+
 let g; // gravity
+let w; //wind
 
 function setup() {
   createCanvas(720, 400);
-  system = new ParticleSystem(createVector(width / 2, 50));
+  // system = new ParticleSystem(createVector(width / 2, 50));
   g = createVector(0, 0.05);
+  w = createVector(0.03, -0.03);
 }
 
 function draw() {
   background(51);
-  system.addParticle();
-  system.applyGravity(g);
-  system.run();
+
+  for(let s of systems){
+    s.addParticle();
+    s.applyGravity(g);
+    s.applyForce(w);
+    s.run();
+  }
+
+}
+
+function mouseClicked(){
+  let mPos = createVector(mouseX, mouseY);
+  let system = new ParticleSystem(mPos);
+
+  systems.push(system);
 }
