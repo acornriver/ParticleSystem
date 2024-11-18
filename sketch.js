ac02ad697bfdc65a8f8641a097ed7163d54d09e5
acornriver <kmicky0806@gmail.com>
2024-11-18 18:59:37 +0900

    펄린노이즈로 worldWind생성
    랜덤으로 변화하는 월드 자체의 바람

--- sketch.js
+++ sketch.js
@@ -1,6 +1,9 @@
 // let system;
 let systems = [];
 
+let t = 0; // 노이즈를 위한 시간 변수
+let worldWind;
+
 let g; // gravity
 let w; //wind
 
@@ -9,14 +12,25 @@ function setup() {
   // system = new ParticleSystem(createVector(width / 2, 50));
   g = createVector(0, 0.05);
   w = createVector(0.09, -0.09);
+
+  worldWind = createVector(0, 0);
 }
 
 function draw() {
   background(51);
 
+
+  // 펄린 노이즈로 바람 생성
+  let windStrengthX = map(noise(t), 0, 1, -0.05, 0.05); // x 방향
+  let windStrengthY = map(noise(t + 100), 0, 1, -0.05, 0.05); // y 방향
+  worldWind.set(windStrengthX, windStrengthY);
+
+  t+= 0.01;
+
   for(let s of systems){
     s.addParticle();
     s.applyGravity(g);
+    s.applyForce(worldWind);
     // s.applyForce(w);
     s.run();
     if(mouseIsPressed){
